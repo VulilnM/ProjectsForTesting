3ba493fc093758465fa0eebd58cf1bcd822e759e
Milan Vulin (Akvelon INC) <v-milanvulin@microsoft.com>
2023-06-20 11:03:35 +0200

    first commit

---
+++ UserService/Models/User.cs
@@ -0,0 +1,10 @@
+namespace UserService.Models
+{
+    public class UserService
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+    }
+}
