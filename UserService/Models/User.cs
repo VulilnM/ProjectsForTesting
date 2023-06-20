b2da246efd8336c6348128bb1520cb30a9970634
Milan Vulin (Akvelon INC) <v-milanvulin@microsoft.com>
2023-06-20 13:05:07 +0200

    Added inMemory db context for user service

--- UserService/Models/User.cs
+++ UserService/Models/User.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace UserService.Models
 {
-    public class UserService
+    public class User
     {
+        [Key]
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string? Name { get; set; }
